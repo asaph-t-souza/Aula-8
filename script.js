6034e1d625a24c75623e2f1e080db28c2ae64ce2
asaph-t-Souza <asaph.net@gmail.com>
2023-01-18 20:31:19 -0300

    Switch

---
+++ script.js
@@ -0,0 +1,13 @@
+const opcao = "Vermelho";
+
+switch(opcao){
+    case "Vermelho": case "Roxo":
+        alert("Botão Vermelho selecionado");
+        break;
+    case "Azul":
+        alert("Botão Azul selecionado");
+        break;
+    default:
+        alert("Opção Invalida");
+        break;
+}
